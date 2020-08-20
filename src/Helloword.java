4597995aa1fae72c8aa6e4ca03308aabe1b90db4
adriel <santos039039039@gmail.com>
2020-08-19 21:14:04 -0300

    Initial commit

---
+++ src/Helloword.java
@@ -0,0 +1,10 @@
+
+
+
+        public class Helloword {
+
+            public static void main(String[] args) {
+                Helloword helloword = new Helloword();
+                Helloword.sayHello();
+            }
+        }
